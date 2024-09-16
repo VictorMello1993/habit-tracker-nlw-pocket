d37ca5362449594cfd05c8aed692fb59458172f9
VictorMello1993 <victorsmello93@gmail.com>
2024-09-15 23:33:25 -0300

    feat: get week pendings goals and get week summary

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
 import { Summary } from "./components/summary";
-// import { EmptyGoals } from "./components/empty-goals";
 import { Dialog } from "./components/ui/dialog";
+import { getSummary } from "./http/get-summary";
+
+type SummaryResponse = {
+	completed: number;
+	total: number;
+	goalsPerDay: Record<
+		string,
+		{
+			id: string;
+			title: string;
+			completedAt: string;
+		}[]
+	>;
+};
 
 export function App() {
+	const { data } = useQuery({
+		queryKey: ["summary"],
+		queryFn: getSummary,
+		staleTime: 1000 * 60, //60seg
+	});
+
 	return (
 		<Dialog>
-			{/* <EmptyGoals /> */}
-			<Summary />
+			{data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 			<CreateGoal />
 		</Dialog>
 	);
